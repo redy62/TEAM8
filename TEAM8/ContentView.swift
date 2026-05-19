e45e4e444ed27fbaee61dfe51b9325efbaa7a1bb
renad <rdz622004@gmail.com>
2026-05-19 16:20:09 +0300

    Initial Commit

---
+++ TEAM8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TEAM8
+//
+//  Created by ريناد محمد حملي on 02/12/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TEAM8/TEAM8App.swift
@@ -0,0 +1,17 @@
+//
+//  TEAM8App.swift
+//  TEAM8
+//
+//  Created by ريناد محمد حملي on 02/12/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct TEAM8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
