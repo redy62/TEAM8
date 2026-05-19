6c5fcc554be3f5e95702659ab7a3b6e634c43d33
renad <rdz622004@gmail.com>
2026-05-19 20:40:30 +0300

    النسخه الاولى

---
+++ TEAM8/Models/ViewModels/Promispage.swift
@@ -0,0 +1,8 @@
+//
+//  Promispage.swift
+//  TEAM8
+//
+//  Created by ريناد محمد حملي on 02/12/1447 AH.
+//
+
+import Foundation
---
+++ TEAM8/Models/ViewModels/Setgoal.swift
@@ -0,0 +1,8 @@
+//
+//  Setgoal.swift
+//  TEAM8
+//
+//  Created by ريناد محمد حملي on 02/12/1447 AH.
+//
+
+import Foundation
